5c058907d7e9704802cd2d8fe4342ab667576306
AshwinGP <111189173+AshwinGP@users.noreply.github.com>
2024-02-28 19:28:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,35 @@
-//complete this code
-class Person {}
+class Person {
+	constructor(name,age){
+       this.name=name;
+		this.age=age;
+		}
+	get getName(){
+		return this.name;
+	}
+	set setAge(age){
+		return this.age=age;
+	}
+}
 
-class Student extends Person {}
+class Student extends Person {
+    
+	study(){
+// 		super(name)
+		console.log(`${this.name} is studying`);
+	}
+}
 
-class Teacher extends Person {}
+class Teacher extends Person {
+	teach(){
+// 		super(name)
+		console.log(`${this.name} is teaching`)
+	}}
+	const person=new Person("John",25);
+	console.log(person.getName);
+	const student = new Student("Alice", 22);
+	const tech=new Teacher("Bob",40);
+console.log(student.study());
+	console.log(tech.teach());
 
 // Do not change the code below this line
 window.Person = Person;
